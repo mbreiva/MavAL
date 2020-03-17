a4b337f6d92e765f10d2d9c79913bc77398ec879
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-03-16 22:04:48 -0400

    Added methods and attributes to Anime class

--- MavAL.py
+++ MavAL.py
@@ -8,19 +8,46 @@ class Anime:
     def __str__(self):
         return self.name
 
+    genreList = []
+    episodeTotal = 0
+    status = ""
+    userRatings = []
+    rating = 0.0
+    relatedSeries = []
+    tagList = []
+
+    def updateGenreList(self, newGenre):
+        self.genreList.append(newGenre)
+
+    def updateRating(self, newRating):
+        self.userRatings.append(newRating)
+        ratingSum = 0
+        for rating in self.userRatings:
+            ratingSum += rating
+        rating = ratingSum / len(self.userRatings)
+
+    def updateStatus(self, newStatus):
+        self.status = newStatus
+
+
+
 class User:
     def __init__(self, name):
         self.name = name
 
     animeList = []
 
-    def addToList(self, anime):
+    def add(self, anime):
         self.animeList.append(anime)
+
+    def remove(self, anime):
+        self.animeList.remove(anime)
 
     def __str__(self):
         animeListStr = ""
         for anime in self.animeList:
-            animeListStr += anime.__str__() + ", "
+            # if first, just add name
+            animeListStr += ", " + anime.__str__()
         return self.name + ": " + animeListStr + "\n"
 
 
@@ -31,4 +58,5 @@ if (__name__ == "__main__"):
     anime2 = Anime("Hunter x Hunter")
     mab.addToList(anime1)
     mab.addToList(anime2)
+    mab.remove(anime1)
     print(mab)
