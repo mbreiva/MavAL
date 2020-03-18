b6270ca6252484e163e7b6eca4c1cbef1e7bc7e8
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-03-17 23:35:54 -0400

    Added Manga class and Manga List attribute to User class

--- MavAL.py
+++ MavAL.py
@@ -29,26 +29,64 @@ class Anime:
     def updateStatus(self, newStatus):
         self.status = newStatus
 
+class Manga:
+    def __init__(self, name):
+        self.name = name
 
+    def __str__(self):
+        return self.name
+
+    genreList = []
+    chapterTotal = 0
+    status = ""
+    userRatings = []
+    rating = 0.0
+    relatedSeries = []
+    tagList = []
+
+    def updateGenreList(self, newGenre):
+        self.genreList.append(newGenre)
+
+    def updateRating(self, newRating):
+        self.userRatings.append(newRating)
+        ratingSum = 0
+        for rating in self.userRatings:
+            ratingSum += rating
+        rating = ratingSum / len(self.userRatings)
+
+    def updateStatus(self, newStatus):
+        self.status = newStatus
 
 class User:
     def __init__(self, name):
         self.name = name
 
     animeList = []
+    mangaList = []
 
-    def add(self, anime):
+    def addAnime(self, anime):
         self.animeList.append(anime)
 
-    def remove(self, anime):
+    def removeAnime(self, anime):
         self.animeList.remove(anime)
+
+    def addManga(self, manga):
+        self.mangaList.append(manga)
+
+    def removeManga(self, manga):
+        self.mangaList.remove(manga)
 
     def __str__(self):
         animeListStr = ""
         for anime in self.animeList:
-            # if first, just add name
-            animeListStr += ", " + anime.__str__()
-        return self.name + ": " + animeListStr + "\n"
+            animeListStr += anime.__str__() + ", "
+
+        mangaListStr = ""
+
+        for manga in self.mangaList:
+            mangaListStr += manga.__str__() + ", "
+
+        return self.name + ": " + animeListStr + "\n" + mangaListStr
 
 
 
@@ -56,7 +94,10 @@ if (__name__ == "__main__"):
     mab = User("maB")
     anime1 = Anime("One Piece")
     anime2 = Anime("Hunter x Hunter")
-    mab.addToList(anime1)
-    mab.addToList(anime2)
-    mab.remove(anime1)
+    manga1 = Manga("One Piece")
+    manga2 = Manga("One Punch Man")
+    mab.addAnime(anime1)
+    mab.addAnime(anime2)
+    mab.addManga(manga1)
+    mab.addManga(manga2)
     print(mab)
