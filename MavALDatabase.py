91dcc13237652ad7dfe24e4e6a1739db13bb707d
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-03-28 23:41:13 -0400

    Added new tables to MavAL database

--- MavALDatabase.py
+++ MavALDatabase.py
@@ -28,10 +28,9 @@ def create_tables():
     commands = (
         """
         CREATE TABLE anime (
-            title               varchar(100) NOT NULL,
+            title               varchar(100) NOT NULL PRIMARY KEY,
             episodes            integer NOT NULL,
             description         varchar,
-            genre               varchar(20),
             rating              decimal,
             releaseDate         date,
             status              varchar(40)
@@ -39,10 +38,9 @@ def create_tables():
         """,
         """ 
         CREATE TABLE manga (
-            title               varchar(100) NOT NULL,
+            title               varchar(100) NOT NULL PRIMARY KEY,
             chapters            integer NOT NULL,
             description         varchar,
-            genre               varchar(20),
             author              varchar(50),
             artist              varchar(50),
             rating              decimal,
@@ -52,17 +50,60 @@ def create_tables():
         """,
         """
         CREATE TABLE user (
+            username            varchar(20) NOT NULL PRIMARY KEY,
             firstName           varchar(40) NOT NULL,
             lastName            varchar(40),
-            username            varchar(20) NOT NULL,
             password            varchar NOT NULL,
             email               varchar,
             creationDate        date NOT NULL
         )
         """,
         """
-        CREATE TABLE genre (
-            category            varchar(20)
+        CREATE TABLE watchRecord (
+            username            varchar(20) FOREIGN KEY REFERENCES user(username),
+            title               varchar(100) FOREIGN KEY REFERENCES anime(title),
+            epWatched           int,
+            watchStatus         varchar(20),
+            userRating          decimal,
+            startDate           date,
+            completionDate      date,
+            comments            varchar,
+            favourite           BOOLEAN NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE readRecord (
+            username            varchar(20) FOREIGN KEY REFERENCES user(username),
+            title               varchar(100) FOREIGN KEY REFERENCES manga(title),
+            chaptersRead        int,
+            readStatus          varchar(20),
+            userRating          decimal,
+            startDate           date,
+            completionDate      date,
+            comments            varchar,
+            favourite           BOOLEAN NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE animeReview (
+            username            varchar(20) FOREIGN KEY REFERENCES user(username),
+            title               varchar(100) FOREIGN KEY REFERENCES anime(title),
+            userRating          decimal,
+            watchStatus         varchar(20), 
+            epWatched           int,
+            date                date,
+            review              varchar
+        )
+        """,
+        """
+        CREATE TABLE mangaReview (
+            username            varchar(20) FOREIGN KEY REFERENCES user(username),
+            title               varchar(100) FOREIGN KEY REFERENCES manga(title),
+            userRating          decimal,
+            watchStatus         varchar(20), 
+            chaptersRead        int,
+            date                date,
+            review              varchar
         )
         """
     )
