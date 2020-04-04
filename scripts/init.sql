2af3acfcd8cd4549cae08483fe0e3a141137e9d7
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-04-03 23:22:24 -0400

    Added unique id to tables and normalized some tables

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE anime (
+   anime_id            serial NOT NULL PRIMARY KEY,
    title               varchar(100) NOT NULL PRIMARY KEY,
-   episodes            integer,
+   episode_total       integer,
    description         varchar,
    rating              decimal,
    release_date        date,
@@ -8,8 +9,9 @@ CREATE TABLE anime (
 );
 
 CREATE TABLE manga (
+    manga_id            serial NOT NULL PRIMARY KEY,
     title               varchar(100) NOT NULL PRIMARY KEY,
-    chapters            integer,
+    chapter_total       integer,
     description         varchar,
     author              varchar(50),
     artist              varchar(50),
@@ -19,62 +21,57 @@ CREATE TABLE manga (
 );
 
 CREATE TABLE maval_user (
-     username            varchar(20) NOT NULL PRIMARY KEY,
-     first_name          varchar(40) NOT NULL,
-     last_name           varchar(40),
+     user_id             serial NOT NULL PRIMARY KEY,
+     username            varchar NOT NULL,
+     first_name          varchar NOT NULL,
+     last_name           varchar,
      password            varchar NOT NULL,
      email               varchar,
      creation_date       date NOT NULL
 );
 
-CREATE TABLE watchRecord (
-     username            varchar(20),
-     title               varchar(100),
+CREATE TABLE anime_record (
+     user_id             serial,
+     anime_id            serial,
      ep_watched          int,
-     watch_status        varchar(20),
+     watch_status        varchar(20) NOT NULL, -- how to make this 3 options?
      user_rating         decimal,
      start_date          date,
      completion_date     date,
      comments            varchar,
      favourite           BOOLEAN NOT NULL,
-     FOREIGN KEY (username) REFERENCES maval_user(username),
-     FOREIGN KEY (title) REFERENCES manga(title)
+     FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
+     FOREIGN KEY (anime_id) REFERENCES manga(anime_id)
 );
 
-CREATE TABLE read_record (
-     username            varchar(20),
-     title               varchar(100),
+CREATE TABLE manga_record (
+     user_id             serial,
+     manga_id            serial,
      chapters_read       int,
-     read_status         varchar(20),
+     read_status         varchar(20) NOT NULL, -- how to make this 3 options?
      user_rating         decimal,
      start_date          date,
      completion_date     date,
      comments            varchar,
      favourite           BOOLEAN NOT NULL,
-     FOREIGN KEY (username) REFERENCES maval_user(username),
-     FOREIGN KEY (title) REFERENCES manga(title)
+     FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
+     FOREIGN KEY (manga_id) REFERENCES manga(manga_id)
 );
 
 CREATE TABLE anime_review (
-      username            varchar(20),
-      title               varchar(100),
-      user_rating         decimal,
-      watch_status        varchar(20),
-      ep_watched          int,
+      user_id             serial,
+      anime_id            serial,
       date_written        date,
       review              varchar,
-      FOREIGN KEY (username) REFERENCES maval_user(username),
-      FOREIGN KEY (title) REFERENCES manga(title)
+      FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
+      FOREIGN KEY (anime_id) REFERENCES manga(anime_id)
 );
 
 CREATE TABLE manga_review (
-       username            varchar(20),
-       title               varchar(100),
-       user_rating         decimal,
-       watch_status        varchar(20),
-       chapters_read       int,
+       user_id             serial,
+       manga_id            serial,
        date_written        date,
        review              varchar,
-       FOREIGN KEY (username) REFERENCES maval_user(username),
-       FOREIGN KEY (title) REFERENCES manga(title)
+       FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
+       FOREIGN KEY (manga_id) REFERENCES manga(manga_id)
 );
