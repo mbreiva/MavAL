6f1c076f9d05fa50f2be89d08b80c0ffd8118876
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-04-04 23:55:31 -0400

    Added functions to Anime and Manga classes for editing properties

--- domain/Anime.py
+++ domain/Anime.py
@@ -1,18 +1,56 @@
 class Anime:
-    def __init__(self, title, episodeTotal, description, releaseDate, status):
+    def __init__(self, title, episodeCount, description, status, releaseDate):
         self.title = title
-        self.episodeTotal = episodeTotal
+        self.episodeCount = episodeCount
         self.description = description
-        self.releaseDate = releaseDate
         self.status = status
+        self.releaseDate = releaseDate
 
   #  def calculateRating(self):
 
-  #  def updateEpisodeTotal(self):
+  # Can't change title because primary key and foreign key referenced by other tables?
 
-  #  def editStatus(self):
+    def updateEpisodeCount(self, cur):
+        newEpisodeCount = input("Episodes: \n")
+        query = ("""UPDATE anime
+                    SET episode_count = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newEpisodeCount, self.title))
 
-  #  def editDescription
+    def editDescription(self, cur):
+        newDescription = input("Description: \n")
+        query = ("""UPDATE anime
+                    SET description = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newDescription, self.title))
+
+    def editStatus(self, cur):
+        newStatus = input("Status: \n")
+        query = ("""UPDATE anime
+                    SET status = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newStatus, self.title))
+
+    def editReleaseDate(self, cur):
+        newReleaseDate = input("Release Date: \n")
+        query = ("""UPDATE anime
+                    SET release_date = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newReleaseDate, self.title))
+
+    def viewReviews(self, cur):
+        query = ("""SELECT * FROM anime_review
+                    WHERE title = %s""")
+        cur.execute(query, self.title)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            # Print review
+            print(f"{row[3]}")
+
+    def watchCount(self, cur):
+
 
     def __str__(self):
-        return self.name
+        return self.title
--- domain/Manga.py
+++ domain/Manga.py
@@ -1,6 +1,50 @@
 class Manga:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, title, chapterTotal, description, author, artist, rating, status, releaseDate):
+        self.name = title
+        self.chapterTotal = chapterTotal
+        self.description = description
+        self.author = author
+        self.artist = artist
+        self.rating = rating
+        self.releaseDate = releaseDate
 
+    def updateChapterCount(self, cur):
+        newChapterCount = input("Chapters: \n")
+        query = ("""UPDATE manga
+                    SET chapter_count = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newChapterCount, self.title))
+
+    def editDescription(self, cur):
+        newDescription = input("Description: \n")
+        query = ("""UPDATE manga
+                    SET description = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newDescription, self.title))
+
+    def editStatus(self, cur):
+        newStatus = input("Status: \n")
+        query = ("""UPDATE manga
+                    SET status = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newStatus, self.title))
+
+    def editReleaseDate(self, cur):
+        newReleaseDate = input("Release Date: \n")
+        query = ("""UPDATE manga
+                    SET release_date = %s
+                    WHERE title = %s""")
+        cur.execute(query, (newReleaseDate, self.title))
+
+    def viewReviews(self):
+        query = ("""SELECT * from manga_review 
+                    WHERE title = %s""")
+        cur.execute(query, self.title)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            # Print review
+            print(f"{row[3]}")
     def __str__(self):
-        return self.name
+        return self.title
