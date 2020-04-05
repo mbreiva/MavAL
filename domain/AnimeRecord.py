841da32be7eae9f0b3aac37593ac1632190bbaf4
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-04-04 23:57:04 -0400

    Added functions for editing properties in AnimeRecord and MangaRecord class

--- domain/AnimeRecord.py
+++ domain/AnimeRecord.py
@@ -1,5 +1,31 @@
 class AnimeRecord:
-    def __init__(self, user, anime, watchStatus):
+    def __init__(self, user, anime, watchStatus, userRating, startDate, completionDate, favourite):
         self.user = user
         self.anime = anime
         self.watchStatus = watchStatus
+        self.userRating = userRating
+        self.startDate = startDate
+        self.completionDate = completionDate
+        self.favourite = favourite
+
+    # destructor?
+
+    # Edit to make user choose between 4 options: Currently watching, On Hold, Dropped, Plan to Watch
+    def editWatchStatus(self, cur):
+        newWatchStatus = input("Watch status: \n")
+
+    def editUserRating(self, cur):
+        newUserRating = input("Rating: \n")
+        query = ("""UPDATE anime_record
+                    SET user_rating = %s
+                    WHERE user_id = %s
+                    AND title = %s""")
+        cur.execute(query, (newUserRating, self.user.userID, self.anime.title))
+
+    def editStartDate(self, cur):
+        newStartDate = input("Start date: \n")
+        query = ("""UPDATE anime_record
+                    SET start_date = %s
+                    WHERE user_id = %s
+                    AND title = %s""")
+        cur.execute(query, (newStartDate, self.user.userID, self.anime.title))
--- domain/MangaRecord.py
+++ domain/MangaRecord.py
@@ -1,5 +1,25 @@
 class MangaRecord:
-    def __init__(self, user, manga, readStatus):
+    def __init__(self, user, manga, readStatus, userRating, startDate, completionDate, favourite):
         self.user = user
         self.manga = manga
         self.readStatus = readStatus
+        self.userRating = userRating
+        self.startDate = startDate
+        self.completionDate = completionDate
+        self.favourite = favourite
+
+    def editUserRating(self, cur):
+        newUserRating = input("Rating: \n")
+        query = ("""UPDATE manga_record
+                    SET user_rating = %s
+                    WHERE user_id = %s
+                    AND title = %s """)
+        cur.execute(query, (newUserRating, self.user.userID, self.manga.title))
+
+    def editStartDate(self, cur):
+        newStartDate = input("Start date: \n")
+        query = ("""UPDATE manga_record
+                    SET start_date = %s
+                    WHERE user_id = %s
+                    AND title = %s""")
+        cur.execute(query, (newStartDate, self.user.userID, self.anime.title))
