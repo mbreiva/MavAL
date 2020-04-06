f50f8128dbdfd8b7b1db6db636fae6d1902ec547
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2020-04-05 23:59:23 -0400

    Updated database with inheritance

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,23 +1,23 @@
-CREATE TABLE anime (
-    anime_id            serial,
-    title               varchar NOT NULL PRIMARY KEY,
-    episode_count       integer,
+CREATE TABLE media (
+    media_id            serial NOT NULL PRIMARY KEY,
+    media_type          varchar NOT NULL,
+    title               varchar NOT NULL,
     description         text,
     rating              decimal,
     status              varchar,
     release_date        date
 );
+CREATE TABLE anime (
+    anime_id            int NOT NULL PRIMARY KEY REFERENCES media(media_id),
+    episode_count       int,
+    studio              varchar
+);
 
 CREATE TABLE manga (
-    manga_id            serial,
-    title               varchar NOT NULL PRIMARY KEY,
-    chapter_count       integer,
-    description         text,
+    manga_id            int NOT NULL PRIMARY KEY REFERENCES media(media_id),
+    chapter_count       int,
     author              varchar,
-    artist              varchar,
-    rating              decimal,
-    status              varchar,
-    release_date        date
+    artist              varchar
 );
 
 CREATE TABLE maval_user (
@@ -25,70 +25,47 @@ CREATE TABLE maval_user (
     username            varchar(50) NOT NULL,
     first_name          varchar(50) NOT NULL,
     last_name           varchar(50),
-    user_password            varchar(30) NOT NULL, --do I have to change name?
+    user_password       varchar(30) NOT NULL, --do I have to change name?
     email               varchar(100),
     creation_date       date NOT NULL
-)
+);
 
-CREATE TABLE anime_record (
-    anime_record_id     serial NOT NULL PRIMARY KEY,
-    user_id             serial,
-    anime_id            serial,
-    ep_watched          int,
-    watch_status        varchar NOT NULL, -- how to make this 3 options?
+CREATE TABLE user_record (
+    record_id           serial NOT NULL PRIMARY KEY,
+    user_id             int NOT NULL,
+    media_id            int NOT NULL,
     user_rating         decimal,
     start_date          date,
     completion_date     date,
-    favourite           BOOLEAN NOT NULL DEFAULT FALSE,
-    isPublic            BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
-    FOREIGN KEY (anime_id) REFERENCES manga(anime_id)
+    isFavourite         boolean NOT NULL DEFAULT FALSE,
+    isPublic            boolean NOT NULL DEFAULT TRUE,
+    FOREIGN KEY(user_id) REFERENCES maval_user(user_id),
+    FOREIGN KEY(media_id) REFERENCES media(media_id)
+);
+
+
+CREATE TABLE anime_record (
+    anime_record_id     serial NOT NULL PRIMARY KEY,
+    record_id           int NOT NULL,
+    episodes_watched    int,
+    FOREIGN KEY(record_id) REFERENCES user_record(record_id)
 );
 
 CREATE TABLE manga_record (
     manga_record_id     serial NOT NULL PRIMARY KEY,
-    user_id             serial,
-    manga_id            serial,
+    record_id           int NOT NULL,
     chapters_read       int,
-    read_status         varchar NOT NULL, -- how to make this 3 options?
-    user_rating         decimal,
-    start_date          date,
-    completion_date     date,
-    favourite           BOOLEAN NOT NULL,
-    isPublic            BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
-    FOREIGN KEY (manga_id) REFERENCES manga(manga_id)
-);
-
-CREATE TABLE anime_review (
-    user_id             serial,
-    anime_id            serial,
-    date_written        date,
-    review              text,
-    FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
-    FOREIGN KEY (anime_id) REFERENCES manga(anime_id)
-);
-
-CREATE TABLE manga_review (
-    user_id             serial,
-    manga_id            serial,
-    date_written        date,
-    review              text,
-    FOREIGN KEY (user_id) REFERENCES maval_user(user_id),
-    FOREIGN KEY (manga_id) REFERENCES manga(manga_id)
+    FOREIGN KEY (record_id) REFERENCES user_record(record_id)
 );
 
 CREATE TABLE review (
-    review_id           serial,
+    review_id           serial NOT NULL PRIMARY KEY,
+    record_id           int NOT NULL,
     date_written        date,
-    review              varchar
-);
-
-CREATE TABLE anime_adaptation (
-    anime_id            serial,
-    manga_id            serial,
-    FOREIGN KEY(anime_id) REFERENCES anime(anime_id),
-    FOREIGN KEY(manga_id) REFERENCES manga(manga_id)
+    review              text,
+    FOREIGN KEY(user_id) REFERENCES maval_user(user_id),
+    FOREIGN KEY(media_id) REFERENCES media(media_id),
+    FOREIGN KEY(record_id) REFERENCES user_record(record_id)
 );
 
 CREATE TABLE genre (
@@ -112,5 +89,27 @@ CREATE TABLE status (
     status_id           serial NOT NULL PRIMARY KEY,
     status              varchar NOT NULL UNIQUE
 );
--- Table for rating?
 
+-- LINK TABLE BETWEEN status AND record
+CREATE TABLE record_status (
+    record_id           int NOT NULL,
+    status_id           int NOT NULL,
+    FOREIGN KEY(record_id) REFERENCES user_record(record_id),
+    FOREIGN KEY(status_id) REFERENCES status(status_id)
+);
+
+-- LINK TABLE BETWEEN MEDIA AND GENRES
+CREATE TABLE media_genre (
+    media_id            int NOT NULL,
+    genre_id            int NOT NULL,
+    FOREIGN KEY(media_id) REFERENCES media(media_id),
+    FOREIGN KEY(genre_id) REFERENCES genre(genre_id)
+);
+
+-- LINK TABLE BETWEEN ANIME AND MANGA
+CREATE TABLE anime_adaptation (
+    anime_id            serial,
+    manga_id            serial,
+    FOREIGN KEY(anime_id) REFERENCES anime(anime_id),
+    FOREIGN KEY(manga_id) REFERENCES manga(manga_id)
+);
