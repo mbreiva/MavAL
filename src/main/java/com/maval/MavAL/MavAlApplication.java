3d19bfc2d2f8faf8a1f97312d940a7d1b5c2b1a3
13abel <therichardwei@gmail.com>
2020-09-06 13:29:35 -0400

    init spring boot

---
+++ src/main/java/com/maval/MavAL/MavAlApplication.java
@@ -0,0 +1,13 @@
+package com.maval.MavAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavAlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavAlApplication.class, args);
+	}
+
+}
