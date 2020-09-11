b13295417d9c1b8d5765088183477fe7bfc2dff0
Mavelyn <mbreiva@uwaterloo.ca>
2020-09-11 14:26:16 -0400

    Added constructors and get/set functions

--- src/main/java/com/maval/MavAL/domain/model/Anime.java
+++ src/main/java/com/maval/MavAL/domain/model/Anime.java
@@ -2,11 +2,36 @@ package com.maval.MavAL.domain.model;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.util.Date;
 
 @Entity
 @DiscriminatorValue("1")
 public class Anime extends Media{
-    public int episodeCount;
-    public String studio;
 
+    public String studio;
+    public int episodeCount;
+
+    public Anime(String title, String studio, int episodeCount, String status, Date releaseDate){
+        this.title = title;
+        this.studio = studio;
+        this.episodeCount = episodeCount;
+        this.status = status;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public int getEpisodeCount() {
+        return episodeCount;
+    }
+
+    public void setEpisodeCount(int episodeCount) {
+        this.episodeCount = episodeCount;
+    }
 }
--- src/main/java/com/maval/MavAL/domain/model/Media.java
+++ src/main/java/com/maval/MavAL/domain/model/Media.java
@@ -7,6 +7,7 @@ import java.util.Date;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name= "mediaType", discriminatorType = DiscriminatorType.INTEGER)
 public class Media {
+
     @Id
     @GeneratedValue
     public int id;
@@ -14,4 +15,40 @@ public class Media {
     public String status;
     public Date releaseDate;
 
+    public Media() {
+    }
+
+    public Media(String title, String status, Date releaseDate) {
+        this.title = title;
+        this.status = status;
+        this.releaseDate = releaseDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus() {
+        this.status = status;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate() {
+        this.releaseDate = releaseDate;
+    }
 }
