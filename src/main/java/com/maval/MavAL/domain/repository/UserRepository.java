7223d4199d1b5a9f645e50e9289e12afc5f6fc79
Mavelyn <mbreiva@uwaterloo.ca>
2020-09-12 00:59:30 -0400

    Added to MangaService

--- src/main/java/com/maval/MavAL/domain/repository/UserRepository.java
+++ src/main/java/com/maval/MavAL/domain/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.maval.MavAL.domain.repository;
 
 import com.maval.MavAL.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {}
