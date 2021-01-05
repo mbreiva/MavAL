08f096717e71710727d0d8bb85c621c8796117e6
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2021-01-05 18:42:17 -0500

    Returned authentication token after login success

--- backend/src/main/java/com/maval/MavAL/controller/LoginRestController.java
+++ backend/src/main/java/com/maval/MavAL/controller/LoginRestController.java
@@ -1,8 +1,10 @@
 package com.maval.MavAL.controller;
 
+import com.maval.MavAL.domain.model.AuthenticationToken;
 import com.maval.MavAL.domain.model.LoginRequestDetails;
 import com.maval.MavAL.domain.model.LoginResponse;
 import com.maval.MavAL.domain.repository.UserRepository;
+import com.maval.MavAL.domain.service.AuthenticationService;
 import com.maval.MavAL.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,6 +21,9 @@ public class LoginRestController {
     @Autowired
     public UserRepository userRepository;
 
+    @Autowired
+    public AuthenticationService authenticationService;
+
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(path = "api/authenticate", consumes = "application/json", produces = "application/json")
     // Returns booleans for each credential
@@ -30,17 +35,20 @@ public class LoginRestController {
         loginResponse.usernameValid = userService.userExistsByUsername(credentials.username);
 
         if(!loginResponse.usernameValid) {
-            loginResponse.passwordValid = false;
+            // TODO: Username does not exist error
             return loginResponse;
         }
 
         // Check if password given matches user's password
         loginResponse.passwordValid = userService.passwordValid(credentials.username, credentials.password);
 
-        if(loginResponse.passwordValid) {
-            loginResponse.id = userRepository.findByUsername(credentials.username).getId();
+        if(!loginResponse.passwordValid) {
+            // TODO: Password invalid error
+            return loginResponse;
         }
 
+        loginResponse.userId = userRepository.findByUsername(credentials.username).getId();
+        loginResponse.authToken.token = authenticationService.createJwtAuthenticationToken(credentials.username);
         return loginResponse;
     }
 }
---
+++ backend/src/main/java/com/maval/MavAL/domain/model/AuthenticationToken.java
@@ -0,0 +1,12 @@
+package com.maval.MavAL.domain.model;
+
+public class AuthenticationToken {
+    public String token;
+
+    public AuthenticationToken() {}
+
+    public AuthenticationToken(String token) {
+        this.token = token;
+    }
+
+}
