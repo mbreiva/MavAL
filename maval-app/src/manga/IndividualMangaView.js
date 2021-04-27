10d52429c8a21a1e783c7419028d21fe0da3497d
Mavelyn Breiva <mbreiva@uwaterloo.ca>
2021-04-26 23:17:07 -0400

    Modified individual manga page design

--- maval-app/src/manga/IndividualMangaView.js
+++ maval-app/src/manga/IndividualMangaView.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core';
 import Container from '@material-ui/core/Container'
 
 const useStyles = makeStyles((theme) =>({
     title: {
-        marginTop: theme.spacing(4),
         marginBottom: theme.spacing(2),
         fontWeight: theme.typography.fontWeightBold,
     },
     paper: {
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(4),
-        display: 'flex',
-        alignItems: 'center',
+        marginLeft: theme.spacing(30),
+        marginRight: theme.spacing(30),
+        marginTop: theme.spacing(4),
+        borderRadius: "20px"
     },
     button: {
         marginTop: theme.spacing(2),
+        margin: "10px",
+        color: "white",
+        backgroundColor: "#50A873"
+    },
+    img: {
+        width: "20%",
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
     }
 }));
 
@@ -30,35 +40,35 @@ export default function IndividualMangaView(props) {
     }
     else {
         mangaInfo = (
-            <div>
-                <Typography component="h1" variant="h4" className={classes.title}>
-                    {props.manga.title}
-                </Typography>
-                <img src={props.manga.imageSource}></img>
-                <p>Status: {props.manga.status}</p>
-                <p>Release Date: {props.manga.releaseDate}</p>
-                <p>Chapters: {props.manga.chapterCount}</p>
-                <Button 
-                    type="submit"
-                    fullWidth
-                    variant="contained" 
-                    color="primary"
-                    className={classes.button}
-                    onClick={props.addUserMedia}
-                >
-                    Add to my list
-                </Button>
-                <Button 
-                    type="submit"
-                    fullWidth
-                    variant="contained" 
-                    color="primary"
-                    className={classes.button}
-                    onClick={props.addMediaToFavourites}
-                >
-                    Favourite
-                </Button>
-            </div>
+            <Paper className={classes.paper} elevation={2}>
+                <Container className={classes.container}>
+                    <Typography component="h1" variant="h4" className={classes.title}>
+                        {props.manga.title}
+                    </Typography>
+                    <img src={props.manga.imageSource} className={classes.img}></img>
+                    <p><span style={{fontWeight: "bold"}}>Status:</span> {props.manga.status}</p>
+                    <p><span style={{fontWeight: "bold"}}>Release Date:</span> {props.manga.releaseDate}</p>
+                    <p><span style={{fontWeight: "bold"}}>Chapters:</span> {props.manga.chapterCount}</p>
+                    <Button 
+                        type="submit"
+                        variant="contained" 
+                        color="primary"
+                        className={classes.button}
+                        onClick={props.addUserMedia}
+                    >
+                        Add to my list
+                    </Button>
+                    <Button 
+                        type="submit"
+                        variant="contained" 
+                        color="primary"
+                        className={classes.button}
+                        onClick={props.addMediaToFavourites}
+                    >
+                        Favourite
+                    </Button>
+                </Container>
+            </Paper>
         );
     }
 
